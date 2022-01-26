5d9a2f6d6635a257a51353703e674e9246a7e3db
Filip Despic <despicpb12@gmail.com>
2022-01-26 11:09:55 +0100

    add random fortunes

--- src/com/luv2/code/springdemo/HappyFortuneService.java
+++ src/com/luv2/code/springdemo/HappyFortuneService.java
@@ -1,8 +1,13 @@
 package com.luv2.code.springdemo;
 
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService{
+	private String[] fortunes = {"Today is your lucky day!", "Good job! Keep it going!", "Good luck!"};
+	
 	@Override
 	public String getFortune() {
-		return "Today is your lucky day!";
+		int rnd = new Random().nextInt(fortunes.length);
+		return fortunes[rnd];
 	}
 }
